092a904018c5a14c35d4792929e0fe53fe3bb61d
Andrey Patukhin <andrey.patukhin@rook1.com>
2020-01-05 18:47:25 +0300

    Terrain, player prefab

---
+++ Assets/Scripts/AxisTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxisTest : MonoBehaviour
+{
+    [SerializeField] CharacterController cc;
+
+    Vector3 gravity = new Vector3(0, -9.7f, 0);
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if(!cc.isGrounded)
+        {
+            cc.Move(gravity);
+        }
+
+    }
+}
